383172ed6af93440702e19b750cca9cb447503fa
ShubhamK-23 <sdkhatavkar22@gmail.com>
2024-07-18 14:01:59 +0530

    minor change

--- TicketNest/src/Pages/Homepage.jsx
+++ TicketNest/src/Pages/Homepage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { Button } from "../components/ui/Button";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "../components/ui/Card";
